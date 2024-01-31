3ced9ddcfa86d84d3864acf106c5d8036459c6c8
Kyrylo <kyrylo.mm@gmail.com>
2024-01-31 08:21:44 +0100

    Completed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
-// Імпорт стилів
-import './index.css';
-
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
-
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+// Імпорт стилів
+import './index.css';
 // Корнева система застосунку
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
